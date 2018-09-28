b0a006bde90569f896744fd71e14cc175723e6b7
PREVEL <ambroise.prevel@capgemini.com>
2018-09-28 23:31:47 +0200

    kjjh

--- script/main.py
+++ script/main.py
@@ -2,9 +2,9 @@ from DataPrep import DataPrep
 
 import pandas as pd
 
-df = pd.read_csv('../data/train_tracking.csv.gz', compression='gzip') 
-target = pd.read_csv('../data/train_session.csv.gz', compression='gzip')
-products = pd.read_csv('../data/dsg18_cdiscount_productid_category.csv.gz', compression='gzip')
+df = pd.read_csv('../../data/train_tracking.csv.gz', compression='gzip') 
+target = pd.read_csv('../../data/train_session.csv.gz', compression='gzip')
+products = pd.read_csv('../../data/dsg18_cdiscount_productid_category.csv.gz', compression='gzip')
 
 products = products.astype(str)
 products = products.loc[products.category_product_id_level1.str.len() <= 2,:]
