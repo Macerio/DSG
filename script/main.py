3594e329067966a2d57de3cdef988746b15e0c09
mcriom <marie-cecile.riom@capgemini.com>
2018-09-28 23:41:08 +0200

    main

--- script/main.py
+++ script/main.py
@@ -2,13 +2,21 @@ from DataPrep import DataPrep
 
 import pandas as pd
 
-df = pd.read_csv('../../data/train_tracking.csv.gz', compression='gzip') 
-target = pd.read_csv('../../data/train_session.csv.gz', compression='gzip')
-products = pd.read_csv('../../data/dsg18_cdiscount_productid_category.csv.gz', compression='gzip')
 
-products = products.astype(str)
-products = products.loc[products.category_product_id_level1.str.len() <= 2,:]
-df = pd.merge(df, products, left_on="sku", right_on="product_id", how="left")
+def main() : 
+    print(">>> Loading Data")
+    df = pd.read_csv('../../data/train_tracking.csv.gz', compression='gzip') 
+    target = pd.read_csv('../../data/train_session.csv.gz', compression='gzip')
+    products = pd.read_csv('../../data/dsg18_cdiscount_productid_category.csv.gz', compression='gzip')
+    
+    print(">>> Starting Data Transformation")
+    products = products.astype(str)
+    products = products.loc[products.category_product_id_level1.str.len() <= 2,:]
+    df = pd.merge(df, products, left_on="sku", right_on="product_id", how="left")
+    
+    
+if __name__ == '__main__':
+    main()
 
 ######## FELIX A TOI DE JOUER #########
 #######################################
