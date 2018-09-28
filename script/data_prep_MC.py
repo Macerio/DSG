02a953b43f2ea498a26f9d3b7b055754847465c5
Félix <felix.revert@outlook.fr>
2018-09-28 22:47:25 +0200

    ajout des data prep personnels

---
+++ script/data_prep_MC.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.base import TransformerMixin
+
+class DataPrepMC(TransformerMixin):
+
+	def __init__():
+		pass
+
+	def fit(X, y=None, **kwargs):
+		return self
+
+	def transform(X, **kwargs):
+		pass
